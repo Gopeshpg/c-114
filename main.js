f78287bcdd9ddf5ad71d7c61c9b21150a65d9ce3
Gopeshpg <98692941+Gopeshpg@users.noreply.github.com>
2023-02-05 12:50:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+
+}
+function draw(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+function draw(){
+}
+function take_snapshot(){
+    save("my_picture.png");
+}
